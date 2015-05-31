f73a3aefad2bc2d04692c91ae964df9a595061ab
Vladimir Zarypov <krre31@gmail.com>
2015-05-31 15:54:12 +0300

    added reading source file by command line argument

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("Sprout version 0.1")
 		fmt.Println("Usage: sprout sourcepath")
+	} else if sourcepath, err := filepath.Abs(os.Args[1]); err != nil {
+		fmt.Println("Error command line parse")
+	} else if output, err := ioutil.ReadFile(sourcepath); err != nil {
+		fmt.Println("Error reading file:", sourcepath)
 	} else {
-		fmt.Println(os.Args[1])
+		fmt.Println(string(output))
 	}
 }
